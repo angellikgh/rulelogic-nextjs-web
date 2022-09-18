9dc9eb723d6f049ed56453b4465cb3a8f0d1f1cd
intelligh <srintelligh@gmail.com>
2022-09-17 20:53:48 -0400

    added env variable

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const withTM = require('next-transpile-modules')(['react-hook-mousetrap']);
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    API_BASE_URL: process.env.API_BASE_URL,
+  },
 };
 
 module.exports = withTM(nextConfig);
