62222445acc0bccc329b55429045565ae1e88ff6
intelligh <srintelligh@gmail.com>
2022-09-20 05:17:57 -0400

    show logo on browser tab

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -46,6 +46,7 @@ MyDocument.getInitialProps = async (ctx) => {
             <meta name='author' content='Marcus Marshall,marcusmrshall' />,
             <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />,
             <meta name='theme-color' content='#000000' />,
+            <link rel="shortcut icon" href="/favicon.png" />,
             <link rel="stylesheet" href="/assets/tailwind-base.css" />,
             <link rel='stylesheet' href='/assets/fonts/inter/inter.css' />,
             <link rel='stylesheet' href='/assets/fonts/meteocons/style.css' />
